7b5ed06a35b44dc49790cdebeaababce38ed8020
Firas S <choogiesaur@users.noreply.github.com>
2023-06-20 14:41:06 -0400

    Alpha BML repack support! And mega code cleanup.

--- bml_util/bml.py
+++ bml_util/bml.py
@@ -2,150 +2,156 @@ import os
 import struct
 import sys
 
-# afs/gsl/bml should generally be the same, 
-# except for the endianness of the file offsets 
-# and sizes for each file entry
-def bml_archive(filename):
- 
+# PRS decompression not implemented at the moment.
+def unpack_bml(filename, unpack_dir):
     if not os.path.exists(filename):
         return None
- 
-    with open(filename, "rb") as fp:
- 
-        fp.seek(0, os.SEEK_END)
-        size = fp.tell()
- 
-        # Parse number of files in archive from file header
-        fp.seek(4)
-        count = struct.unpack('i', fp.read(4))[0]
-        fp.seek(0x40)
- 
-        files = []
-        for i in range(count):
-            data = fp.read(0x34)
-            s = struct.unpack('>32sIIIII', data)
-            fp.read(0x0c)
- 
-            name = s[0].decode('ascii').rstrip(' \t\r\n\0')
-            compressed_size = s[1]
-            file_type = s[2]
-            decompressed_size = s[3]
-            pvm_compressed_size = s[4]
-            pvm_decompressed_size = s[5]
- 
-            files.append({
-                "filename" : name,
-                "compressed_size" : compressed_size,
-                "decompressed_size" : decompressed_size
-            })
- 
-            if pvm_compressed_size == 0:
-                continue
- 
-            # File name without extension
-            basename = os.path.splitext(name)[0]
- 
-            files.append({
-                "filename" : "%s.pvm" % (basename),
-                "compressed_size" : pvm_compressed_size,
-                "decompressed_size" : pvm_decompressed_size
-            })
- 
-        ofs = fp.tell()
-        ofs = (0xFFFFF800 & ofs) + 0x800;
-        fp.seek(ofs)
- 
-        bml = []
-        for file in files:
- 
-            # Seek to next non-zero byte
-            while fp.tell() < size:
-                byte = struct.unpack('B', fp.read(1))[0]
-                if byte == 0:
-                    continue
-                fp.seek(-1, os.SEEK_CUR)
-                break
- 
-            # Read and decompress the file by reading amount of bytes for this file discovered in file header
-            bytes = fp.read(file["compressed_size"])
-            bml.append({
-                "filename" : file["filename"],
-                "bytes" : prs_decompress(bytes)
-            })
- 
-        return bml
 
-# Extract the first 64 bytes, BML header
-def extract_header(fp):
+    with open(filename, "rb") as file:
+        file_size = os.path.getsize(filename)
 
-    # struct bml_header_s {
-    #   uint8_t unused[4];
-    #   uint32_t num_files;
-    #   uint32_t unk; // 0x00000150, magic number?
-    #   uint8_t unused2[52];
-    # };
+        file.seek(4)
+        file_count = struct.unpack('i', file.read(4))[0]
+        file.seek(0x40)
 
-    fp.seek(0, os.SEEK_END)
-    size = fp.tell()
-    print("Size:", size)
+        bml_entries = []
+        for _ in range(file_count):
+            entry_data = file.read(0x34)
+            entry = struct.unpack('>32sIIIII', entry_data)
+            file.read(0x0c)
 
-    fp.seek(0)
-    header_bytes = fp.read(64)
-    s = struct.unpack('>4sII52s', header_bytes)
+            name = entry[0].decode('ascii').rstrip(' \t\r\n\0')
+            compressed_size = entry[1]
+            decompressed_size = entry[3]
+            pvm_compressed_size = entry[4]
+            pvm_decompressed_size = entry[5]
 
-    return s
+            entry_info = {
+                "filename": name,
+                "compressed_size": compressed_size,
+                "decompressed_size": decompressed_size
+            }
 
-# Extract a 64 byte file entry at given offset
-def extract_file_entry(fp, offset):
+            if pvm_compressed_size:
+                basename = os.path.splitext(name)[0]
+                pvm_entry = {
+                    "filename": f"{basename}.pvm",
+                    "compressed_size": pvm_compressed_size,
+                    "decompressed_size": pvm_decompressed_size
+                }
+                bml_entries.append(pvm_entry)
 
-    # struct bml_file_table_entry_s {
-    #   uint8_t  filename[32];
-    #   uint32_t compressed_size;
-    #   uint32_t unused1;
-    #   uint32_t decompressed_size;
-    #   uint32_t gvm_compressed_size;   # if this 
-    #   uint32_t gvm_decompressed_size; # and this are > 0, there is a .gvm following this file
-    #   uint8_t  unused[12]
-    # };
+            bml_entries.append(entry_info)
 
-    fp.seek(offset)
-    print("0x"+str(fp.tell()))
+        bml_data = []
+        offset = file.tell()
+        offset = (0xFFFFF800 & offset) + 0x800
+        file.seek(offset)
 
-    entry_bytes = fp.read(64)
-    s = struct.unpack('>32sIIIII12s', entry_bytes)
+        while file.tell() < file_size:
+            byte = struct.unpack('B', file.read(1))[0]
+            if byte != 0:
+                file.seek(-1, os.SEEK_CUR)
+                bytes_data = file.read(bml_entries[len(bml_data)]["compressed_size"])
+                bml_data.append({
+                    "filename": bml_entries[len(bml_data)]["filename"],
+                    # PRS decompression not implemented at the moment.
+                    # "bytes": prs_decompress(bytes_data)
+                    "bytes": bytes_data
+                })
 
-    return s
+        for file_info in bml_data:
+            file_path = os.path.join(unpack_dir, file_info["filename"])
+            with open(file_path, "wb") as output_file:
+                output_file.write(file_info["bytes"])
 
-def print_file_entry(file_entry):
-    print(file_entry[0].decode('ascii').rstrip(' \t\r\n\0'))
-    print('compressed_size:',file_entry[1])
-    print('unused1:',file_entry[2])
-    print('decompressed_size:',file_entry[3])
-    print('gvm_compressed_size:',file_entry[4])
-    print('gvm_decompressed_size:',file_entry[5])
-    print()
+        order_file = os.path.join(unpack_dir, "order.txt")
+        with open(order_file, "w") as order_fp:
+            for file_info in bml_data:
+                order_fp.write(file_info["filename"] + "\n")
+
+        return bml_data
+
+def pack_bml(unpack_dir, filename, order_file):
+    print("Packing:", unpack_dir, "to file:", filename)
+    with open(order_file, "r") as order_fp:
+        file_names = [line.strip() for line in order_fp.readlines()]
+
+    contents = []
+    offset = 64 + (len(file_names) * 64)
+
+    for item in file_names:
+        path = os.path.join(unpack_dir, item)
+        with open(path, "rb") as f:
+            raw_bytes = f.read()
+            contents.append({"filename": item, "bytes": raw_bytes})
+
+            file_entry = struct.pack(
+                ">32sIIIII12s",
+                item.encode("ascii"),
+                len(raw_bytes),
+                0,
+                len(raw_bytes),
+                0,
+                0,
+                b"\x00" * 12,
+            )
+
+            offset += len(raw_bytes)
+            offset += 2048 - (len(raw_bytes) % 2048)
+
+    with open(filename, "wb") as out:
+        out.write(struct.pack(">4sII52s", b"\x00" * 4, len(file_names), 0x150, b"\x00" * 52))
+
+        for file_entry in contents:
+            out.write(file_entry["filename"].encode("ascii")[:32])
+            out.write(struct.pack(">I", len(file_entry["bytes"])))
+            out.write(struct.pack(">I", 0))
+            out.write(struct.pack(">I", len(file_entry["bytes"])))
+            out.write(struct.pack(">I", 0))
+            out.write(struct.pack(">I", 0))
+            out.write(b"\x00" * 12)
+
+            out.write(file_entry["bytes"])
+
+        padding_size = (2048 - (offset % 2048)) % 2048
+        out.write(b"\x00" * padding_size)
+
+    print("BML archive repacked:", filename)
 
 def main():
+    if len(sys.argv) < 2:
+        print("Usage: python script.py <bml_file>")
+        return
 
     filename = sys.argv[1]
 
     if not os.path.exists(filename):
-        return None
+        print(f"Error: File '{filename}' does not exist.")
+        return
 
-    with open(filename, "rb") as fp:
+    print(f"Processing BML archive: {filename}\n")
 
-        # Extract BML header
-        header = extract_header(fp)
-        file_count = header[1]
-        print('File Count:', file_count, '\n')
+    # Extracting BML archive
+    unpack_dir = os.path.splitext(filename)[0]
+    os.makedirs(unpack_dir, exist_ok=True)
+    extracted_files = unpack_bml(filename, unpack_dir)
 
-        # Starting right after header, extract n=file_count file_entries
-        offset = 64
-        for i in range(file_count):
-            file_entry = extract_file_entry(fp, offset)
+    if extracted_files is None:
+        print("Error: Failed to extract BML archive.")
+    else:
+        print("Extracted files:")
+        for file_info in extracted_files:
+            print(file_info["filename"])
 
-            print_file_entry(file_entry)
-            offset += 64
+        if len(sys.argv) >= 5 and sys.argv[2] == "-p":
+            new_bml_filename = sys.argv[3]
+            order_file = sys.argv[4]
+
+            # Repack the extracted files into a new BML archive
+            pack_bml(unpack_dir, new_bml_filename, order_file)
+            print(f"\nBML archive repacked: {new_bml_filename}")
+
 
 if __name__ == "__main__":
     main()
